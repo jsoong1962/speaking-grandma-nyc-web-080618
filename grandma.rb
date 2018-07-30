262abfc432cf340d23c19200bf46348371d45552
Johnny Soong <johnnysoong@hotmail.com>
2018-07-30 19:26:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == string.upcase
-     return "NO, NOT SINCE1938"
-  elseif !(string == "I LOVE YOU GrANDMA1")
+     return "NO, NOT SINCE 1938!"
+  elseif (string == "I LOVE YOU GRANDMA!")
      return "I LOVE YOU TOO PUMPKIN!"
   else
      return "HUH?! SPEAK UP, SONNY!"
